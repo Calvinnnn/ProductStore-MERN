8d6a5a8b1d514c74ae57eb60240471622d8c5ea4
Omar Emad <oemad3366@gmail.com>
2026-01-14 16:54:44 +0200

    Api has been created Post , delete , Get methods has been added to interace with Product Model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/dp.js';
+import Product from './modles/product.model.js';
 dotenv.config();
 const app = express();
 
@@ -12,8 +13,51 @@ app.listen(5000,()=>{
 
 });
 
+app.use(express.json()); //allow to use accept json data in the req.body
+
 app.get("/",(req,res)=>{
     res.send("Product List");
+});
+
+app.post("/api/products", async (req,res)=>{
+    const product = req.body;
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({succsess:false,message:"All fields are required"});
+    }
+    const newProduct = new Product(product);
+    try{
+        await newProduct.save();
+        res.status(201).json({succsess:true,data:newProduct});
+
+    }catch(error){
+        console.log("Error in creating product:",error.message)
+        res.status(500).json({succsess:false,message:"Server Error"});
+    }
+});
+
+
+app.delete("/api/products/:id",async (req,res)=>{
+   const {id} = req.params;
+   console.log("Deleting product with id:",id);
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({succsess:true,message:"Product deleted successfully"});
+    }
+    catch(error){
+        console.log("Error in deleting product:",error.message)
+        res.status(500).json({succsess:false,message:"Server Error"});
+    }
+});
+
+
+app.get("/api/products",async (req,res)=>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json({succsess:true,data:products});
+    }catch(error){
+        console.log("Error in fetching products:",error.message)
+        res.status(500).json({succsess:false,message:"Server Error"});
+    }   
 });
 
 console.log(process.env.MONGO_URI);
