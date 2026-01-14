a880dd916c5b72cc2fb1820e601b645b3e29aeab
Omar Emad <oemad3366@gmail.com>
2026-01-14 16:04:00 +0200

    Data base has been created and product scehma and model has been created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from 'express';
-
+import dotenv from "dotenv";
+import { connectDB } from './config/dp.js';
+dotenv.config();
 const app = express();
 
 
+
 app.listen(5000,()=>{
+    connectDB();
     console.log("Server started at http://localhost:5000");
-})
+
+});
+
+app.get("/",(req,res)=>{
+    res.send("Product List");
+});
+
+console.log(process.env.MONGO_URI);
+
